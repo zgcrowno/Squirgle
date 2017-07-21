25548db9172e11a1738ba1891767326ad0f270ce
Zachary Crownover <zgcrowno@gmail.com>
2017-07-21 17:31:29 -0500

    Initial commit

---
+++ desktop/src/com/screenlooker/squirgle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.screenlooker.squirgle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.screenlooker.squirgle.Squirgle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Squirgle(), config);
+	}
+}
---
+++ html/src/com/screenlooker/squirgle/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.screenlooker.squirgle.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.screenlooker.squirgle.Squirgle;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Squirgle();
+        }
+}
