1e434c14965d412ec1a3bee52301b37f5668ed5d
Zachary Crownover <zgcrowno@gmail.com>
2017-08-05 11:03:26 -0500

    Got drawPrompt method off the ground

--- core/src/com/screenlooker/squirgle/Draw.java
+++ core/src/com/screenlooker/squirgle/Draw.java
@@ -8,25 +8,164 @@ import com.badlogic.gdx.math.Vector2;
 import static com.badlogic.gdx.graphics.Color.BLACK;
 
 public class Draw {
-    private float pointRadius = 10;
-    private float lineWidth = 12.5f;
-    private float inputDistanceOffset = (float) 1.5;
-    private int inputRadius = 50;
-    private Vector2 inputPointSpawn = new Vector2(Gdx.graphics.getWidth() / 5, (inputDistanceOffset * inputRadius));
-    private Vector2 inputLineSpawn = new Vector2((2 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
-    private Vector2 inputTriangleSpawn = new Vector2((3 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
-    private Vector2 inputSquareSpawn = new Vector2((4 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
+    private float pointRadius;
+    private float lineWidth;
+    private float inputDistanceOffset;
+    private int inputRadius;
+    private Vector2 inputPointSpawn;
+    private Vector2 inputLineSpawn;
+    private Vector2 inputTriangleSpawn;
+    private Vector2 inputSquareSpawn;
+    Vector2 inputTriangleTop;
+    Vector2 inputTriangleLeft;
+    Vector2 inputTriangleRight;
+    Vector2 inputSquareTopLeft;
+    Vector2 inputSquareBottomLeft;
+    Vector2 inputSquareBottomRight;
+    Vector2 inputSquareTopRight;
+    Vector2 promptTriangleTop;
+    Vector2 promptTriangleLeft;
+    Vector2 promptTriangleRight;
+    Vector2 promptSquareTopLeft;
+    Vector2 promptSquareBottomLeft;
+    Vector2 promptSquareBottomRight;
+    Vector2 promptSquareTopRight;
 
-    public void drawPrompt(float x, float y, int shape, float initShapeSize, ShapeRenderer shapeRenderer) {
-        if(shape == 0) {
+    public Draw() {
+        pointRadius = 0;
+        lineWidth = 0;
+        inputDistanceOffset = 0;
+        inputRadius = 0;
+        inputPointSpawn = new Vector2();
+        inputLineSpawn = new Vector2();
+        inputTriangleSpawn = new Vector2();
+        inputSquareSpawn = new Vector2();
+        inputTriangleTop = new Vector2();
+        inputTriangleLeft = new Vector2();
+        inputTriangleRight = new Vector2();
+        inputSquareTopLeft = new Vector2();
+        inputSquareBottomLeft = new Vector2();
+        inputSquareBottomRight = new Vector2();
+        inputSquareTopRight = new Vector2();
+        promptTriangleTop = new Vector2();
+        promptTriangleLeft = new Vector2();
+        promptTriangleRight = new Vector2();
+        promptSquareTopLeft = new Vector2();
+        promptSquareBottomLeft = new Vector2();
+        promptSquareBottomRight = new Vector2();
+        promptSquareTopRight = new Vector2();
+    }
 
-        } else if(shape == 1) {
+    public Draw(float screenWidth) {
+        pointRadius = 10;
+        lineWidth = 12.5f;
+        inputDistanceOffset = 1.5f;
+        inputRadius = 50;
+        inputPointSpawn = new Vector2(screenWidth / 5, (inputDistanceOffset * inputRadius));
+        inputLineSpawn = new Vector2((2 * screenWidth) / 5, (inputDistanceOffset * inputRadius));
+        inputTriangleSpawn = new Vector2((3 * screenWidth) / 5, (inputDistanceOffset * inputRadius));
+        inputSquareSpawn = new Vector2((4 * screenWidth) / 5, (inputDistanceOffset * inputRadius));
+        inputTriangleTop = new Vector2(inputTriangleSpawn.x, inputTriangleSpawn.y + (inputRadius / 2));
+        inputTriangleLeft = new Vector2(inputTriangleSpawn.x - (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2));
+        inputTriangleRight = new Vector2(inputTriangleSpawn.x + (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2));
+        inputSquareTopLeft = new Vector2(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2));
+        inputSquareBottomLeft = new Vector2(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2));
+        inputSquareBottomRight = new Vector2(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2));
+        inputSquareTopRight = new Vector2(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2));
+        promptTriangleTop = new Vector2();
+        promptTriangleLeft = new Vector2();
+        promptTriangleRight = new Vector2();
+        promptSquareTopLeft = new Vector2();
+        promptSquareBottomLeft = new Vector2();
+        promptSquareBottomRight = new Vector2();
+        promptSquareTopRight = new Vector2();
+    }
 
-        } else if(shape == 2) {
+    public float getPointRadius() {
+        return pointRadius;
+    }
 
+    public void setPointRadius(float pointRadius) {
+        this.pointRadius = pointRadius;
+    }
+
+    public float getLineWidth() {
+        return lineWidth;
+    }
+
+    public void setLineWidth(float lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
+    public float getInputDistanceOffset() {
+        return inputDistanceOffset;
+    }
+
+    public void setInputDistanceOffset(float inputDistanceOffset) {
+        this.inputDistanceOffset = inputDistanceOffset;
+    }
+
+    public int getInputRadius() {
+        return inputRadius;
+    }
+
+    public void setInputRadius(int inputRadius) {
+        this.inputRadius = inputRadius;
+    }
+
+    public Vector2 getInputPointSpawn() {
+        return inputPointSpawn;
+    }
+
+    public void setInputPointSpawn(Vector2 inputPointSpawn) {
+        this.inputPointSpawn = inputPointSpawn;
+    }
+
+    public Vector2 getInputLineSpawn() {
+        return inputLineSpawn;
+    }
+
+    public void setInputLineSpawn(Vector2 inputLineSpawn) {
+        this.inputLineSpawn = inputLineSpawn;
+    }
+
+    public Vector2 getInputTriangleSpawn() {
+        return inputTriangleSpawn;
+    }
+
+    public void setInputTriangleSpawn(Vector2 inputTriangleSpawn) {
+        this.inputTriangleSpawn = inputTriangleSpawn;
+    }
+
+    public Vector2 getInputSquareSpawn() {
+        return inputSquareSpawn;
+    }
+
+    public void setInputSquareSpawn(Vector2 inputSquareSpawn) {
+        this.inputSquareSpawn = inputSquareSpawn;
+    }
+
+    public void drawPrompt(float x, float y, int promptShape, float promptSize, Color color, ShapeRenderer shapeRenderer) {
+        if(promptShape == 0) {
+            drawPoint(x, y, promptSize, color, shapeRenderer);
+        } else if(promptShape == 1) {
+            drawLine(x, y + (2 * promptSize), x, y - (2 * promptSize), promptSize, color, shapeRenderer);
+        } else if(promptShape == 2) {
+            promptTriangleTop.set(x, y + (1.5f * promptSize));
+            promptTriangleLeft.set(x - (1.5f * promptSize), y - (1.5f * promptSize));
+            promptTriangleRight.set(x + (1.5f * promptSize), y - (1.5f * promptSize));
+            drawTriangle(promptTriangleTop, promptTriangleLeft, promptTriangleRight, promptSize, color, shapeRenderer);
         } else {
-
+            promptSquareTopLeft.set(x - (1.5f * promptSize), y + (1.5f * promptSize));
+            promptSquareBottomLeft.set(x - (1.5f * promptSize), y - (1.5f * promptSize));
+            promptSquareBottomRight.set(x + (1.5f * promptSize), y - (1.5f * promptSize));
+            promptSquareTopRight.set(x + (1.5f * promptSize), y + (1.5f * promptSize));
+            drawSquare(promptSquareTopLeft, promptSquareBottomLeft, promptSquareBottomRight, promptSquareTopRight, promptSize, color, shapeRenderer);
         }
+    }
+
+    public void drawPreviousPrompts() {
+
     }
 
     public void drawPoint(float x, float y, float radius, Color color, ShapeRenderer shapeRenderer) {
@@ -79,20 +218,11 @@ public class Draw {
         shapeRenderer.setColor(Color.WHITE);
         shapeRenderer.circle(inputTriangleSpawn.x, inputTriangleSpawn.y, inputRadius);
 
-        Vector2 inputTriangleTop = new Vector2(inputTriangleSpawn.x, inputTriangleSpawn.y + (inputRadius / 2));
-        Vector2 inputTriangleLeft = new Vector2(inputTriangleSpawn.x - (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2));
-        Vector2 inputTriangleRight = new Vector2(inputTriangleSpawn.x + (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2));
-
         drawTriangle(inputTriangleTop, inputTriangleLeft, inputTriangleRight, inputRadius / 4, Color.BLACK, shapeRenderer);
 
         //Square
         shapeRenderer.setColor(Color.WHITE);
         shapeRenderer.circle(inputSquareSpawn.x, inputSquareSpawn.y, inputRadius);
-
-        Vector2 inputSquareTopLeft = new Vector2(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2));
-        Vector2 inputSquareBottomLeft = new Vector2(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2));
-        Vector2 inputSquareBottomRight = new Vector2(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2));
-        Vector2 inputSquareTopRight = new Vector2(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2));
 
         shapeRenderer.rectLine(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputRadius / 4);
         shapeRenderer.rectLine(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputRadius / 4);
--- core/src/com/screenlooker/squirgle/Squirgle.java
+++ core/src/com/screenlooker/squirgle/Squirgle.java
@@ -9,28 +9,68 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Squirgle extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private ShapeRenderer shapeRenderer;
+	private Draw draw;
+	private float inputDistanceOffset;
+	private float promptSize;
+	private int promptShape;
+	private int inputRadius;
+	private List<Integer> promptShapeList;
+	private List<Double> promptSizeList;
+	private List<Color> promptColorList;
 	private Vector2 promptShapeSpawn;
 	private Vector2 inputPointSpawn;
 	private Vector2 inputLineSpawn;
 	private Vector2 inputTriangleSpawn;
 	private Vector2 inputSquareSpawn;
-	private float inputDistanceOffset = (float) 1.5;
-	private float initShapeSize = 10;
-	private int inputRadius = 50;
-	private int point = 0;
-	private int line = 1;
-	private int triangle = 2;
-	private int square = 3;
+	private int point;
+	private int line;
+	private int triangle;
+	private int square;
+	private int red;
+	private int blue;
+	private int green;
+	private int yellow;
+	private int magenta;
+	private int cyan;
 	
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 768, 1024);
+		shapeRenderer = new ShapeRenderer();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		draw = new Draw(Gdx.graphics.getWidth());
+		inputDistanceOffset = (float) 1.5;
+		promptSize = 10;
+		promptShape = MathUtils.random(3);
+		inputRadius = 50;
+		promptShapeList = new ArrayList<Integer>();
+		promptSizeList = new ArrayList<Double>();
+		promptColorList = new ArrayList<Color>();
+		promptShapeSpawn = new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		inputPointSpawn = new Vector2(Gdx.graphics.getWidth() / 5, (inputDistanceOffset * inputRadius));
+		inputLineSpawn = new Vector2((2 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
+		inputTriangleSpawn = new Vector2((3 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
+		inputSquareSpawn = new Vector2((4 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
+		point = 0;
+		line = 1;
+		triangle = 2;
+		square = 3;
+		red = 0;
+		blue = 1;
+		green = 2;
+		yellow = 3;
+		magenta = 4;
+		cyan = 5;
 	}
 
 	@Override
@@ -44,59 +84,11 @@ public class Squirgle extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 
-		promptShapeSpawn = new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		inputPointSpawn = new Vector2(Gdx.graphics.getWidth() / 5, (inputDistanceOffset * inputRadius));
-		inputLineSpawn = new Vector2((2 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
-		inputTriangleSpawn = new Vector2((3 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
-		inputSquareSpawn = new Vector2((4 * Gdx.graphics.getWidth()) / 5, (inputDistanceOffset * inputRadius));
-
-		shapeRenderer = new ShapeRenderer();
-		shapeRenderer.setProjectionMatrix(camera.combined);
-
-		//Draw the prompt shape
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.circle(promptShapeSpawn.x, promptShapeSpawn.y, initShapeSize);
 
-		//Draw the input buttons
+		draw.drawPrompt(promptShapeSpawn.x, promptShapeSpawn.y, promptShape, promptSize, Color.BLACK, shapeRenderer);
 
-		//Point
-		shapeRenderer.setColor(Color.WHITE);
-		shapeRenderer.circle(inputPointSpawn.x, inputPointSpawn.y, inputRadius);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.circle(inputPointSpawn.x, inputPointSpawn.y, inputRadius / 4);
-
-		//Line
-		shapeRenderer.setColor(Color.WHITE);
-		shapeRenderer.circle(inputLineSpawn.x, inputLineSpawn.y, inputRadius);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.rectLine(inputLineSpawn.x, inputLineSpawn.y - (inputRadius / 2), inputLineSpawn.x, inputLineSpawn.y + (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputLineSpawn.x, inputLineSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputLineSpawn.x, inputLineSpawn.y + (inputRadius / 2), inputRadius / 4);
-
-		//Triangle
-		shapeRenderer.setColor(Color.WHITE);
-		shapeRenderer.circle(inputTriangleSpawn.x, inputTriangleSpawn.y, inputRadius);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.rectLine(inputTriangleSpawn.x, inputTriangleSpawn.y + (inputRadius / 2), inputTriangleSpawn.x - (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.rectLine(inputTriangleSpawn.x - (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputTriangleSpawn.x + (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.rectLine(inputTriangleSpawn.x, inputTriangleSpawn.y + (inputRadius / 2), inputTriangleSpawn.x + (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputTriangleSpawn.x, inputTriangleSpawn.y + (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputTriangleSpawn.x - (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputTriangleSpawn.x + (inputRadius / 2), inputTriangleSpawn.y - (inputRadius / 2), inputRadius / 4);
-
-		//Square
-		shapeRenderer.setColor(Color.WHITE);
-		shapeRenderer.circle(inputSquareSpawn.x, inputSquareSpawn.y, inputRadius);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.rectLine(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.rectLine(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.rectLine(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.rectLine(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputSquareSpawn.x - (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y - (inputRadius / 2), inputRadius / 4);
-		shapeRenderer.circle(inputSquareSpawn.x + (inputRadius / 2), inputSquareSpawn.y + (inputRadius / 2), inputRadius / 4);
+		draw.drawInputButtons(inputRadius, shapeRenderer);
 
 		shapeRenderer.end();
 
