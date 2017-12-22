c43201d098d483cca9a9706e9eee617811e318e5
zachary <zgcrowno@gmail.com>
2017-12-21 20:33:00 -0600

    Got Squirgle running in IOS Simulator and on iPad

--- ios/src/com/screenlooker/squirgle/IOSLauncher.java
+++ ios/src/com/screenlooker/squirgle/IOSLauncher.java
@@ -1,11 +1,16 @@
 package com.screenlooker.squirgle;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.screenlooker.squirgle.Squirgle;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIApplicationOpenURLOptions;
+import org.robovm.pods.facebook.core.FBSDKAppEvents;
+import org.robovm.pods.facebook.core.FBSDKApplicationDelegate;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
@@ -18,5 +23,25 @@ public class IOSLauncher extends IOSApplication.Delegate {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
+    }
+
+    @Override
+    public void didBecomeActive(UIApplication application) {
+        super.didBecomeActive(application);
+        FBSDKAppEvents.activateApp();
+    }
+
+
+    @Override
+    public boolean openURL(UIApplication app, NSURL url, UIApplicationOpenURLOptions options) {
+        return FBSDKApplicationDelegate.getSharedInstance().openURL(app, url, options.getSourceApplication(), options.getAnnotation());
+
+    }
+
+    @Override
+    public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
+        boolean finished = super.didFinishLaunching(application, launchOptions);
+        FBSDKApplicationDelegate.getSharedInstance().didFinishLaunching(application, launchOptions);
+        return finished;
     }
 }
